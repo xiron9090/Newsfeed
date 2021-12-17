38f627c268e078ecfc8edc2c84afbd65526ed91a
xiron9090 <xiron9090@gmail.com>
2021-12-17 00:14:20 -0500

    feat: Add folder structure and intial config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: ['NEWS_API_kEY'],
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
---
+++ src/core/api/index.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://newsapi.org/v2/';
+const TOP_HEADLINES = `${BASE_URL}top-headlines`;
+
+export default {
+  BASE_URL,
+  TOP_HEADLINES,
+};
